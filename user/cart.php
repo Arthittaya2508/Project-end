89a793c1590c3e866207ed27e5a366649766a9bf
ATOMMIC <122683207+Arthittaya2508@users.noreply.github.com>
2024-03-18 14:12:35 +0700

    user commit

--- user/cart.php
+++ user/cart.php
@@ -10,20 +10,19 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ploynappan</title>
-    <!-- Bootstrap CSS -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Option 1: Bootstrap Bundle with Popper -->
+
     <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- CSS -->
-    <link rel="stylesheet" href="cart.css">
-
+    <!-- <link rel="stylesheet" href="cart.css"> -->
+    <link rel="stylesheet" href="up_cart.css">
 
 </head>
 
 <body>
     <?php include 'menu.php';?>
     <br><br>
-    <div class="container  ">
+    <div class="container">
         <form id="form1" method="POST" action="insert_cart.php">
             <div class="row">
                 <div class="col">
@@ -33,12 +32,13 @@ session_start();
 
                     <table class="table table-striped table-hover">
                         <tr>
-                            <th>ลำดับที่</th>
-                            <th>ชื่อสินค้า</th>
+                            <th><input type="checkbox" id="selectAllCheckbox" aria-label="Checkbox for selecting all">
+                            </th>
+                            <th class="centered-cell">เลือกสินค้าทั้งหมด</th>
+                            <th></th>
                             <th>ราคาสินค้า</th>
                             <th>จำนวนสินค้า</th>
                             <th>ราคารวม</th>
-                            <th>เพิ่ม - ลด</th>
                             <th>ลบสินค้า</th>
                         </tr>
                         <?php
@@ -65,36 +65,44 @@ for ($i=0; $i <= (int)$_SESSION["intLine"]; $i++){
 
 ?>
                         <tr>
-                            <td><?=$m?></td>
-                            <td>
+                            <td><input type="checkbox" class="itemCheckbox"
+                                    aria-label="Checkbox for following text input">
+                            </td>
+
+                            <!-- <td><?=$m?></td> -->
+                            <td class="centered-cell">
                                 <img src="img/<?=$row_pro['image']?>" width="80" height="85" class="border">
                                 <?=$row_pro['pro_name']?>
                             </td>
+                            <td class="null">สินค้าคงเหลือ (<?php echo $row_pro['amount']; ?>)</td>
                             <td><?=$row_pro['price']?></td>
-                            <td><?=$_SESSION["strQty"][$i]?></td>
-                            <td><?=$sum?></td>
+
+
                             <td>
                                 <?php
-               // Assuming $row_pro is the current product's data
-            $productID = $row_pro['pro_id']; // Product ID
+    $productID = $row_pro['pro_id']; 
 
-              // Retrieve product information from the database
-            $sql_product = "SELECT * FROM product WHERE pro_id = '$productID'";
-            $result_product = mysqli_query($conn, $sql_product);
-            $row_product = mysqli_fetch_assoc($result_product);
+    $sql_product = "SELECT * FROM product WHERE pro_id = '$productID'";
+    $result_product = mysqli_query($conn, $sql_product);
+    $row_product = mysqli_fetch_assoc($result_product);
 
-            $maxStock = $row_product['amount']; // Maximum available stock
+    $maxStock = $row_product['amount']; 
 
-            if ($_SESSION["strQty"][$i] < $maxStock) {
-    echo '<a href="order.php?id=' . $row_pro['pro_id'] . '" class="btn btn-success">+</a>';
-}
-            if ($_SESSION["strQty"][$i] > 1) {
-    echo '<a href="order_del.php?id=' . $row_pro['pro_id'] . '" class="btn btn-danger">-</a>';
-}
+    if ($_SESSION["strQty"][$i]) {
+        echo '<a id="decrement" class="button1" onclick="decrementAmount()" href="order_del.php?id=' . $row_pro['pro_id'] . '">-</a>';
+    }
+
+echo '<div id="amount-container"> <span  id="amount">' . $_SESSION["strQty"][$i] . '</span></div>';
+    
+    
+    if ($_SESSION["strQty"][$i] ) {
+        echo '<a id="increment" class="button1" onclick="incrementAmount()"  href="order.php?id=' . $row_pro['pro_id'] . '">+</a>';
+    }
 ?>
+                            <td><?=$sum?></td>
                             </td>
-                            <td class="text-center"><a href="pro_delete.php?Line=<?=$i?>"> <img
-                                        src="img/rubbishbin2.png" width="30"> </a></td>
+                            <td class="delete"><a href="pro_delete.php?Line=<?= $i ?>"><i class="fas fa-trash"></i></a>
+                            </td>
                         </tr>
                         <?php
  $m=$m+1;
@@ -114,13 +122,181 @@ mysqli_close($conn);
                     <div style="text-align:right">
                         <a href="all_products.php"> <button type="button"
                                 class="btn btn-outline-secondary">เลือกสินค้า</button> </a>
-                        <button type="submit" class="btn btn-outline-success">ยืนยันการสั่งซื้อ</button>
+                        <!-- Button trigger modal -->
+
+                        <button id="orderButton" type="button" class="btn btn-outline-success" data-bs-toggle="modal"
+                            data-bs-target="#exampleModal">สั่งซื้อสินค้า </button>
+
+                        <script>
+                        function showExistingAddress() {
+                            document.getElementById('existingAddressForm').classList.remove('hide');
+                            document.getElementById('newAddressForm').classList.add('hide');
+                            document.getElementById('fullname').readOnly = true;
+                            document.getElementById('tel').readOnly = true;
+                        }
+
+                        function showNewAddress() {
+                            document.getElementById('newAddressForm').classList.remove('hide');
+                            document.getElementById('existingAddressForm').classList.add('hide');
+                            document.getElementById('fullname').readOnly = false;
+                            document.getElementById('tel').readOnly = false;
+
+                        }
+                        </script>
+                        <?php
+include 'condb.php';
+
+
+// เรียกข้อมูลที่อยู่และเบอร์โทรศัพท์ของผู้ใช้งานที่ล็อกอิน
+$username = $_SESSION['username'];
+$sql = "SELECT * FROM member WHERE username = '$username'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $name = $row['name'];
+        $lastname = $row['lastname']; // เพิ่มการดึงนามสกุล
+        $address = $row['address'];
+        $telephone = $row['telephone'];
+        $fullname = $name . " " . $lastname; // รวมชื่อและนามสกุลเข้าด้วยกัน
+    }
+}
+
+?>
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                            aria-hidden="true">
+
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">เลือกที่อยู่สำหรับจัดส่ง</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="but">
+                                            <button class="form-check form-check-inline"
+                                                onclick="showExistingAddress()">
+                                                <input class="form-check-input" type="radio" name="addressOption"
+                                                    id="existingAddressOption" value="existing" checked>
+                                                <label class="form-check-label"
+                                                    for="existingAddressOption">ที่อยู่ที่มีอยู่ในระบบ</label>
+                                            </button>
+                                            <button class="form-check form-check-inline" onclick="showNewAddress()">
+                                                <input class="form-check-input" type="radio" name="addressOption"
+                                                    id="newAddressOption" value="new">
+                                                <label class="form-check-label"
+                                                    for="newAddressOption">แก้ไขข้อมูลที่อยู่</label>
+                                            </button>
+                                        </div>
+                                        <br><br>
+
+
+                                        <form id="form01" method="POST" action="in_address.php">
+                                            <div class="row1">
+                                                <!-- เพิ่มฟิลด์ชื่อ-นามสกุล -->
+                                                <div class="text02">
+                                                    <label for="fullname" class="text02">
+                                                        ชื่อ-นามสกุล
+                                                    </label>
+                                                    <input type="text" class="form-control" id="fullname"
+                                                        name="fullname"
+                                                        value="<?php echo htmlspecialchars($fullname); ?>" readonly>
+                                                </div>
+
+                                                <div id="existingAddressForm">
+                                                    <div class="text02">
+                                                        <label for="existingAddress" class="text02">
+                                                            ที่อยู่ที่มีอยู่ในระบบ
+                                                        </label>
+                                                        <input type="text" class="form-control" id="existingAddress"
+                                                            name="existingAddress"
+                                                            value="<?php echo htmlspecialchars($address); ?>" readonly>
+                                                    </div>
+                                                    <hr>
+                                                </div>
+
+                                                <div id="newAddressForm" class="hide">
+                                                    <div class="text02">
+                                                        <label for="newAddress" class="text02">
+                                                            ที่อยู่ใหม่ (กรอกเฉพาะกรณีที่ต้องการเปลี่ยนที่อยู่)
+                                                        </label>
+                                                        <textarea class="form-control" id="newAddress" name="newAddress"
+                                                            rows="3"><?php echo htmlspecialchars($address); ?></textarea>
+                                                    </div>
+                                                    <hr>
+                                                </div>
+
+                                                <div class="text02">
+                                                    <label for="tel" class="text02">
+                                                        เบอร์โทรศัพท์
+                                                    </label>
+                                                    <input type="tel" class="form-control" id="tel" name="tel"
+                                                        value="<?php echo htmlspecialchars($telephone); ?>" readonly>
+                                                </div>
+                                            </div>
+
+                                            <!-- <button type="submit" class="sm">Submit</button> -->
+                                        </form>
+
+
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary"
+                                            data-bs-dismiss="modal">ปิด</button>
+                                        <button type="submit" class="btn btn-primary">ยืนยัน</button>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
-                <br>
-                </from>
-
             </div>
+        </form>
+    </div>
+    <script>
+    // Function to check if any item is selected
+    function checkSelectedItems() {
+        var checkboxes = document.querySelectorAll('.itemCheckbox');
+        var selected = false;
+        checkboxes.forEach(function(checkbox) {
+            if (checkbox.checked) {
+                selected = true;
+            }
+        });
+        return selected;
+    }
+
+    // Function to show notification if no item is selected
+    function showNotification() {
+        var notification = document.getElementById('notification');
+        if (!checkSelectedItems()) {
+            notification.style.display = 'block';
+        } else {
+            notification.style.display = 'none';
+        }
+    }
+
+    // Listen for checkbox changes
+    var checkboxes = document.querySelectorAll('.itemCheckbox');
+    checkboxes.forEach(function(checkbox) {
+        checkbox.addEventListener('change', function() {
+            showNotification();
+            calculateTotal(); // Recalculate total when checkbox changes
+        });
+    });
+    // Function to handle checkbox toggle
+    document.getElementById('selectAllCheckbox').addEventListener('click', function() {
+        var checkboxes = document.querySelectorAll('.itemCheckbox');
+        checkboxes.forEach(function(checkbox) {
+            checkbox.checked = document.getElementById('selectAllCheckbox').checked;
+        });
+        showNotification(); // Check if any item is selected after toggling select all
+        calculateTotal(); // Recalculate total when select all checkbox changes
+    });
+    </script>
 </body>
 
 </html>
--- user/insert_cart.php
+++ user/insert_cart.php
@@ -2,11 +2,11 @@
 session_start();
 include 'condb.php';
 
-// Check if any product is selected before confirming the order
-if (!isset($_SESSION["intLine"]) || $_SESSION["intLine"] <= 0) {
-    echo "<script>alert('กรุณาเลือกสินค้าก่อนยืนยันคำสั่งซื้อ'); window.location='cart.php';</script>";
-    exit; // Stop further execution
-}
+// // Check if any product is selected before confirming the order
+// if (!isset($_SESSION["intLine"]) || $_SESSION["intLine"] <= 0) {
+//     echo "<script>alert('กรุณาเลือกสินค้าก่อนยืนยันคำสั่งซื้อ'); window.location='cart.php';</script>";
+//     exit; // Stop further execution
+// }
 
 $cusName = $_POST['cus_name'];
 $cusAddress = $_POST['cus_add'];
@@ -50,5 +50,25 @@ unset($_SESSION["strProductID"]);
 unset($_SESSION["strQty"]);
 unset($_SESSION["sum_price"]);
 
-echo "<script>alert('บันทึกข้อมูลเรียบร้อยแล้ว'); window.location='address.php';</script>";
+
+
+include 'condb.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // ตรวจสอบและกำหนดค่าตัวแปร session สำหรับชื่อ-นามสกุล, ที่อยู่ และเบอร์โทรศัพท์
+    $_SESSION["fullname"] = $_POST["fullname"];
+
+    // ตรวจสอบว่าผู้ใช้เลือกใช้ที่อยู่ที่มีอยู่ในระบบหรือเพิ่มที่อยู่ใหม่
+    if ($_POST["addressOption"] == "existing") {
+        $_SESSION["address"] = $_POST["existingAddress"];
+    } else {
+        $_SESSION["address"] = $_POST["newAddress"];
+    }
+
+    $_SESSION["telephone"] = $_POST["tel"];
+
+    // Redirect ไปยังหน้า orderNum.php
+    header("Location: orderNum.php");
+    exit();
+}
 ?>
